f2c8c815e959aeac88e5e95b77572c02fe288529
Abdulloh <abdullohabduhamidov@gmail.com>
2022-11-17 16:22:28 +0500

    js day 2

---
+++ day -2 js/main.js
@@ -0,0 +1,23 @@
+var usd = 500;
+var usd$ = 250;
+var yevroSon = 120;
+
+// $ usd
+
+var dooler = 10650.34;
+
+// yevro
+
+var yevo = 10650.03;
+
+var hamsi = (usd + usd$) * dooler + yevroSon * yevo;
+var jami = hamsi;
+
+console.log(`umumuiy xarjatiz ${jami}`);
+
+var age = prompt("sizda hisobizda 9265758.6 so'm puliyiz bo'ish kerak");
+if (age < 9265758.6) {
+  alert("siznig hisobiz yetarli mablak yo'q");
+} else if (age > 9265758.6) {
+  alert("sizda yetarlicha mablaq mavjut");
+}
